0282de8e252d0a3d60428953bdfb9deb14ab7c90
John Regan <john@jrjrtech.com>
2020-02-23 14:26:00 -0500

    gme: support sgc files

--- src/decoder/plugins/GmeDecoderPlugin.cxx
+++ src/decoder/plugins/GmeDecoderPlugin.cxx
@@ -349,6 +349,9 @@ gme_container_scan(Path path_fs)
 static const char *const gme_suffixes[] = {
 	"ay", "gbs", "gym", "hes", "kss", "nsf",
 	"nsfe", "sap", "spc", "vgm", "vgz",
+#ifdef ENABLE_GME_KODE54
+	"sgc",
+#endif
 	nullptr
 };
 
