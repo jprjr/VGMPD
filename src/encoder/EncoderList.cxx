9dbc2fc07b0b1d2e535296dce200fa19cbc070a9
John Regan <john@jrjrtech.com>
2020-09-09 10:00:07 -0400

    add new "technicallyflac" encoder plugin

--- src/encoder/EncoderList.cxx
+++ src/encoder/EncoderList.cxx
@@ -29,6 +29,7 @@
 #include "plugins/ShineEncoderPlugin.hxx"
 #include "plugins/LameEncoderPlugin.hxx"
 #include "plugins/TwolameEncoderPlugin.hxx"
+#include "plugins/technicallyflac/TechnicallyFlacEncoderPlugin.hxx"
 #include "decoder/Features.h"
 
 #include <string.h>
@@ -55,6 +56,9 @@ const EncoderPlugin *const encoder_plugins[] = {
 #endif
 #ifdef ENABLE_SHINE
 	&shine_encoder_plugin,
+#endif
+#ifdef ENABLE_TECHNICALLYFLAC
+	&technicallyflac_encoder_plugin,
 #endif
 	nullptr
 };
